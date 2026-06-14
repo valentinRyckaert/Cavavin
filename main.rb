158868fa0643fd6d7900f437f50586fe40d0e275
valentin <valentin.ryckaert@protonmail.com>
2026-06-14 14:44:52 +0200

    init: refacto

--- main.rb
+++ main.rb
@@ -3,15 +3,26 @@ require 'sinatra'
 require 'csv'
 
 get '/' do
-    erb :index
+    erb :index, locals: { listeVins: nil, action: nil }
 end
 
-get '/search' do
+get '/search/:action' do |action|
     listeVins = []
-    CSV.foreach("db/database.csv", headers: true, skip_blanks: true) do |row|
-        if row[params["searchType"]].include?(params['query'])
-            listeVins.push(row)
+    csvData = CSV.read("db/database.csv", headers: true, skip_blanks: true)
+    if action === 'consume'
+        csvData.each do |row|
+            if row[params["searchType"]].include?(params['query'])
+                listeVins.push(row)
+            end
         end
+    elsif action === 'add'
+        csvData.each do |row|
+            if row["appelation"].include?(params['appelation']) && row["annee"].to_i === params["annee"].to_i
+                listeVins.push(row)
+            end
+        end
+    else
+        halt 404, 'no route found' 
     end
-    erb :index, locals: { listeVins: listeVins }
+    erb :index, locals: { listeVins: listeVins, action: action }
 end
