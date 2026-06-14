b6757d53b04e9a46213df4a5063af0affed6cfb0
valentin <valentin.ryckaert@protonmail.com>
2026-06-14 13:38:09 +0200

    feat: add search functionality

--- main.rb
+++ main.rb
@@ -7,5 +7,11 @@ get '/' do
 end
 
 get '/search' do
-    
+    listeVins = []
+    CSV.foreach("db/database.csv", headers: true, skip_blanks: true) do |row|
+        if row["appelation"].include?(params['query'])
+            listeVins.push(row)
+        end
+    end
+    erb :index, locals: { data: listeVins }
 end
